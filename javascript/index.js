e7519cc5acd9a788c264fcc81f0844b528945378
kblizzy22 <kbailey322@gmail.com>
2024-06-03 15:01:23 -0400

    fixed cityName variable

--- javascript/index.js
+++ javascript/index.js
@@ -32,15 +32,13 @@ function updateCity(event) {
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city">
               <div>
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>
-        <div class="city" id="dublin">
-          <div>`;
+        </div>`;
 }
 
 updateTime();
